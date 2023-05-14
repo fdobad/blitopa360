a9a91e512220ed70dc7318c54934fef49e9c0e18
fdobad <8339628-fdobad@users.noreply.gitlab.com>
2023-05-14 13:36:12 -0400

    fixv0

--- attempts/scri.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-#"${1}/exiftool" -api largefilesupport -ee -q -m -p insv_gpx.fmt -ext insv $2 > "${2}/insv.gpx"
-"${1}/exiftool" -api largefilesupport -ee -q -m -p insp_gpx.fmt -ext insp $2 > "${2}/insp.gpx"
